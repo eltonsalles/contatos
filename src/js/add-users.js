ffcd0081275c336cc59f030df1805b8b6447b172
Elton Rodrigues <elton.leite@multilaser.com.br>
2019-02-20 17:50:52 -0300

    Alterado o layout do botão para carregar mais usuários.

--- src/js/add-users.js
+++ src/js/add-users.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
   let buttonAddUser = document.querySelector('.add-user');
   buttonAddUser.addEventListener('click', function () {
+    this.classList.add('invisible');
     getUsers();
   });
 });
